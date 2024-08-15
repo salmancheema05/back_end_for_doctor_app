acbf03d35e890e093b67c9bb3925c548739017c1
salmancheema05 <59694522+salmancheema05@users.noreply.github.com>
2024-08-15 04:30:10 -0700

    added correct path of middleware in routes

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { signup, login, logout, accessToken } from "./controllers/user.js";
-import { verifyUserToken } from "./middleware/verifyToken.js";
+import { verifyUserToken } from "./middleware/verifyusertoken.js";
 
 const route = express.Router();
 route.post("/api/signup", signup);
